2e56397c4442f4f5f31d6bab5088a6699638d04b
elirod <rodrigomacade@gmail.com>
2023-05-06 21:29:50 -0300

    remove default color

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <Box align="center" opacity={0.4} fontSize="sm">
       <Text>
         &copy; {new Date().getFullYear()} This website is built based on the {' '}
-        <Link href='https://www.craftz.dog/' target='_blank' color="teal">
+        <Link href='https://www.craftz.dog/' target='_blank'>
           Takuya Matsuyama&apos;s website.  
         </Link> 
         &nbsp;All Rights Reserved.
